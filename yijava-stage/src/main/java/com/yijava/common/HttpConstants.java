c471f542fe387c1f55e1d48800a9b3dab4384e9e
zhjt <zhjt_88@163.com>
2013-09-17 18:47:19 +0800

    upload update

--- yijava-stage/src/main/java/com/yijava/common/HttpConstants.java
+++ yijava-stage/src/main/java/com/yijava/common/HttpConstants.java
@@ -3,16 +3,16 @@ package com.yijava.common;
 public class HttpConstants {
 
 	//获取所有省份
-	public static String GET_PROVINCE_URI="http://219.237.241.77/cnc/province.jsp";
+	public static String GET_PROVINCE_URI="http://data.cncnews.cn/cnc/province.jsp";
 	
 	//获取区域新闻的接口
-	public static String GET_NEW_BYPROVINCE_URI="http://219.237.241.77/cnc/localsearch.jsp";
+	public static String GET_NEW_BYPROVINCE_URI="http://data.cncnews.cn/cnc/localsearch.jsp";
 	
 	//获取所有栏目
-	public static String GET_ALLCOLUMN="http://219.237.241.77/cnc/column_list.jsp";
+	public static String GET_ALLCOLUMN="http://data.cncnews.cn/cnc/column_list.jsp";
 	
 	//根据栏目获取新闻
-	public static String GET_NEW_BYCOLUMN_URI="http://219.237.241.77/cnc/column_news_list.jsp";
+	public static String GET_NEW_BYCOLUMN_URI="http://data.cncnews.cn/cnc/column_news_list.jsp";
 
 	
 	//得到订阅
@@ -22,5 +22,5 @@ public class HttpConstants {
 	
 	public static String GET_ISLOGIN_URI="http://login.cncnews.cn/user/islogin.cc";
 	
-	public static String SERVER_BASE_URL="http://manage.yijava.com/";
+	public static String SERVER_BASE_URL="http://audio.cncnews.cn/stream/";
 }
